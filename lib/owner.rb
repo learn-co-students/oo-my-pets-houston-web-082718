679b7752bd96a691eda3f2fca730b12d4681e15a
v10let <violet.wmoon@gmail.com>
2018-08-30 13:38:43 -0500

    many classes, many pets

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,77 @@
 class Owner
-  # code goes here
+	attr_accessor :name, :count, :reset_all, :pets
+	attr_reader :species, :say_species
+
+@@all = []
+	
+	def initialize(name)
+		@name = name
+		@species = "human"
+		@pets = {fishes: [], dogs: [], cats: []}
+		@@all << self
+	end
+
+	def self.all
+		@@all
+	end
+
+	def self.count
+		@@all.length
+	end
+
+	def self.reset_all
+		@@all = []
+	end
+
+	def species
+		@species
+	end
+
+	def say_species
+		return "I am a human."
+	end
+
+	def buy_fish(name)
+		@pets[:fishes] << Fish.new(name)
+	end
+
+	def buy_cat(name)
+		@pets[:cats] << Cat.new(name)
+	end
+
+	def buy_dog(name)
+		@pets[:dogs] << Dog.new(name)
+	end
+
+	def walk_dogs
+		@pets[:dogs].map { |dog|
+			dog.mood = "happy"
+		}
+	end
+
+	def play_with_cats
+		@pets[:cats].map { |cat|
+			cat.mood = "happy"
+		}
+	end
+
+	def feed_fish
+		@pets[:fishes].map { |fish|
+			fish.mood = "happy"
+		}
+	end
+
+	def sell_pets
+		@pets.each { |type, pets|
+			pets.each { |pet|
+				pet.mood = "nervous"
+			}
+		} 
+		@pets = []
+	end
+
+	def list_pets
+		return "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
+		
+	end
 end
